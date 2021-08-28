c17f2a47e2452b23755c5b342f9bfa8e99f98a1c
Jae Young Choi <jaeyoung@Jaes-Mac-mini.home>
2021-08-28 16:28:44 -0400

    small

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const sections = document.getElementsByTagName('section');
+const sections = document.getElementsByTagName("section");
 
 for (let i = 0; i < sections.length; i++) {
-	// adjust vertical rootMargin to account for sections that are taller than the viewport
-	// the difference between section and viewport height is divided by 2 since the margin applies to both top and bottom
-	const margin = (sections[i].clientHeight - document.documentElement.clientHeight) / 2;
+  // adjust vertical rootMargin to account for sections that are taller than the viewport
+  // the difference between section and viewport height is divided by 2 since the margin applies to both top and bottom
+  const margin =
+    (sections[i].clientHeight - document.documentElement.clientHeight) / 2;
 
-	const config = {
-		threshold: 0.75,
-		rootMargin: `${margin}px 0px`
-	};
+  const config = {
+    threshold: 0.75,
+    rootMargin: `${margin}px 0px`,
+  };
 
-	const observer = new IntersectionObserver((entries) => {
-		for (let i = 0; i < entries.length; i++) {
-			if (entries[i].isIntersecting) {
-				const tgt = entries[i].target;
-				tgt.classList.add('in');
-				observer.unobserve(tgt);
-			}
-		}
-	}, config);
+  const observer = new IntersectionObserver((entries) => {
+    for (let i = 0; i < entries.length; i++) {
+      if (entries[i].isIntersecting) {
+        const tgt = entries[i].target;
+        tgt.classList.add("in");
+        observer.unobserve(tgt);
+      }
+    }
+  }, config);
 
-	observer.observe(sections[i]);
+  observer.observe(sections[i]);
 }
